1351b5b364dede12488f4ec35f9f3a78d1a14f7b
tntobias <tntobias@gmail.com>
2019-06-26 23:43:50 -0400

    initial commit

---
+++ examples/termchat/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/blushft/rc"
+)
+
+func clientOpts() []rc.ClientOption {
+	return []rc.ClientOption{
+		rc.ServerURL("https://chat.dev.rpxapps.com"),
+		rc.CredFromJson("../../token.json"),
+		rc.Debug(true),
+		rc.Realtime(true),
+	}
+}
+
+func createTestClient() *rc.Client {
+	opts := clientOpts()
+	return rc.New(opts...)
+}
+
+func main() {
+	client := createTestClient()
+	if err := client.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	subs, err := client.SubscribeToRoomMessages("__my_messages__")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case m := <-subs.Updates:
+			msgs, ok := m.([]rc.RoomMessage)
+			if !ok {
+				log.Fatalf("this isn't messages? %v", m)
+			}
+			log.Printf("%d messages\n", len(msgs))
+			for _, msg := range msgs {
+				log.Printf("From: %s - %s\n", msg.User.Username, msg.Msg)
+			}
+		case serr := <-subs.Errors:
+			log.Fatal(serr)
+		}
+	}
+}
